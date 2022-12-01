832c6e036e0e0aad2945de5b402e11bc9ab5733f
schen044 <ninjarulers@yahoo.com>
2022-12-01 07:38:10 -0800

    Adds CRUD for goals

--- src/utilities/goals-api.js
+++ src/utilities/goals-api.js
@@ -9,3 +9,15 @@ export function getAll() {
 export function getById(id) {
     return sendRequest(`${BASE_URL}/${id}`)
 }
+
+export async function addGoal(formData) {
+    return sendRequest(`${BASE_URL}/add`, 'POST', formData)
+}
+
+export async function editGoal(id, formData) {
+    return sendRequest(`${BASE_URL}/${id}`, 'PUT', formData)
+}
+
+export function deleteGoal(id) {
+    return sendRequest(`${BASE_URL}/${id}`, 'DELETE')
+}
