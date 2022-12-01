8c48563c722db77efb9d90a4732d1895867c0244
schen044 <ninjarulers@yahoo.com>
2022-12-01 09:26:57 -0800

    Deploy

--- src/components/GoalListItem/GoalListItem.jsx
+++ src/components/GoalListItem/GoalListItem.jsx
@@ -22,9 +22,9 @@ export default function GoalListItem({ goalItem }) {
   return (
     <div className="GoalListItem">
       <div className="name">
+        <input type="checkbox" defaultChecked={goalItem.completed} className="complete-box"/>
         <img src={goalItem.icon} />
         {goalItem.name}
-        <input type="checkbox" defaultChecked={goalItem.completed}/>
         <Link to={`/goals/edit/${itemId}`}><button type="submit">Edit</button></Link>
         <button type="submit" onClick={handleDelete}>Delete</button>
       </div>
